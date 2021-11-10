1d502d0f45b9b01cc387b1ae6710b2a69d5ecf6b
DHARSHITHA1 <91359009+DHARSHITHA1@users.noreply.github.com>
2021-11-10 16:13:16 +0530

    Add files via upload

---
+++ SpringBootWebDataJpaLibrary/src/main/java/spboot/web/jpa/demo/SpringBootWebDataJpaLibraryApplication.java
@@ -0,0 +1,13 @@
+package spboot.web.jpa.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebDataJpaLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebDataJpaLibraryApplication.class, args);
+	}
+
+}
